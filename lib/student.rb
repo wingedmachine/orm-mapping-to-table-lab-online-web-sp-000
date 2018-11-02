858c55abbe6471675f00be7078fd6dc125f8259b
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 14:20:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,10 @@
 class Student
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  attr_reader :name, :grade
 
-  def initialize
-    
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
   end
 end
